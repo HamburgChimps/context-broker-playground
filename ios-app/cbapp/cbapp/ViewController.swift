7526446153bbd1e4708377bb16ed640c1bd2b3cb
Alex Olson <alexkellenolson@gmail.com>
2019-08-23 14:29:02 +0200

    all parts of initial project now in git

---
+++ ios-app/cbapp/cbapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cbapp
+//
+//  Created by Alex Olson on 8/23/19.
+//  Copyright © 2019 HamburgChimps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
